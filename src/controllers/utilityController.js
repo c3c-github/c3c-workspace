3e46a15bd09a0bc288f92b107867aadaf230c3cd
C3C Software <71038887+c3c-github@users.noreply.github.com>
2026-03-02 08:54:59 -0300

    feat: use local assets for signature generator

--- src/controllers/utilityController.js
+++ src/controllers/utilityController.js
@@ -1,6 +1,10 @@
 exports.renderSignaturePage = (req, res) => {
+    // Protocolo e host dinâmicos para URLs absolutas de imagens
+    const baseUrl = `${req.protocol}://${req.get('host')}`;
+
     res.render('utilities_signature', { 
         user: req.session.user, 
-        page: 'utilities_signature' 
+        page: 'utilities_signature',
+        baseUrl: baseUrl
     });
 };
